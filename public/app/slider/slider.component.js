d61e196e54c6a9c7b5ce930477e46f3a96cf4858
Gabriel Muller <ernesto.gabriel90@gmail.com>
2016-08-01 17:10:07 -0300

    implement transition with css/js.

--- public/app/slider/slider.component.js
+++ public/app/slider/slider.component.js
@@ -3,7 +3,7 @@
 
 
 	var SliderCompiler = function(elem, attrs) {
-		var	width = parseInt($('.item').css('width'))
+		var	width = parseInt($('.slide').css('width'))
 			counter = 0;
 		return {
 			// POSTLINK
@@ -30,45 +30,48 @@
 						body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit nam, vero iure nulla, dolores possimus ex, voluptatem pariatur tenetur beatae odio nisi nihil voluptas. Laudantium quaerat error, numquam nostrum nulla?'
 					}
 				];
+				
 
 				scope.$next = function() {
-					if( isLastOne() ) {
-						reset();
-					} else {
-						counter -= width;
-						move(counter);
-						$('.active')
-						.removeClass('active').next().first().addClass('active')
+					counter -= width;
+					
+					if( _isLastOne() ) {
+						elem.find('.slide-next').attr('disabled', true);
+					}else{
+						_move(counter);
+						$('.current').removeClass('current').next().first().addClass('current');
 					}
 				}
-
+				
 				scope.$prev = function () {
 					counter += width;
-					if( isFirstOne() ) {
-						reset();
+					if( _isfirstOne() ){
+						elem.find('.slide-prev').attr('disabled', true);
+					} else {
+						_move(counter);
+						$('.current').removeClass('current').prev().addClass('current');
 					}
-					move(counter);
+
 				}
 
-				function move(x) {
-					$('.item').animate({
-						'left': (x +'px')
-					});
+				function _isfirstOne() {
+					return $('.first').hasClass('current');
 				}
 
-				function reset() {
-					$('.item').css('left', 0);
-					counter = width;
+				function _move(x) {
+					$('.slider-track').css({
+						'transform': 'translate('+x+'px,0)'
+					})
 				}
 
-				function isLastOne() {
-					var length = $('.item').length;
-					return $('[slide-index="'+(length-1)+'"]').hasClass('active');
+				function _reset() {
+					_move(0);
 				}
 
-				function isFirstOne() {
-					return $('[slide-index="0"]').hasClass('active');
+				function _isLastOne() {
+					return $('.last').hasClass('current');
 				}
+
 			}
 		}
 	}
