b3588c0ba6b4994035bec95cd3526f1e025aefdc
Gabriel Muller <ernesto.gabriel90@gmail.com>
2016-08-16 16:20:07 -0300

    mv index to public/ added small update to main.component

--- public/app/main/main.component.js
+++ public/app/main/main.component.js
@@ -1,6 +1,40 @@
 (function(ng){
 	var module = ng.module('todoApp', ['slider-component']);
 
+	var MainCtrl = function(scope){
+		scope.vm = this;
+
+		this.items = [
+			{
+				id:0,
+				title: 'SLIDE 1',
+				body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit nam, vero iure nulla, dolores possimus ex, voluptatem pariatur tenetur beatae odio nisi nihil voluptas. Laudantium quaerat error, numquam nostrum nulla?'
+			},
+			{
+				id:1,
+				title: 'SLIDE 2',
+				body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit nam, vero iure nulla, dolores possimus ex, voluptatem pariatur tenetur beatae odio nisi nihil voluptas. Laudantium quaerat error, numquam nostrum nulla?'
+			},
+			{
+				id:2,
+				title: 'SLIDE 3',
+				body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit nam, vero iure nulla, dolores possimus ex, voluptatem pariatur tenetur beatae odio nisi nihil voluptas. Laudantium quaerat error, numquam nostrum nulla?'
+			},
+			{
+				id:3,
+				title: 'SLIDE 4',
+				body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit nam, vero iure nulla, dolores possimus ex, voluptatem pariatur tenetur beatae odio nisi nihil voluptas. Laudantium quaerat error, numquam nostrum nulla?'
+			},
+			{
+				id:4,
+				title: 'SLIDE 5',
+				body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit nam, vero iure nulla, dolores possimus ex, voluptatem pariatur tenetur beatae odio nisi nihil voluptas. Laudantium quaerat error, numquam nostrum nulla?'
+			}
+		];
+	}
+
+	MainCtrl.$inject = ['$scope'];
+	
 	var MainDirective = function(){
 		return {
 			scope: {
@@ -8,10 +42,11 @@
 				course: '@'
 			},
 			templateUrl: './app/main/main.html',
-			restrict: 'AE'
+			restrict: 'AE',
+			controller: 'MainCtrl'
 		}
 	};
 
-	module.directive('main', MainDirective);
+	module.controller('MainCtrl', MainCtrl).directive('main', MainDirective);
 
 })(angular);
